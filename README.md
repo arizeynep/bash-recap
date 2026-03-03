90e571989468352c44fedadc4118737bc80e92ea
Zeynep <melisaari81@gmail.com>
2026-03-03 14:22:13 +0200

    Added comments for reverse string problem.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bash-recap
 Repository used to recap my bash knowledge.
+
+1. Reverse strings (read them from right to left, rather than from left to right).
--- reverse_string.sh
+++ reverse_string.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+# - Symbol # is used to get the length of the word.
+# - The following parameter expansion was used in the "for" loop: ${var:offset:length}, 
+# which returns a sub-string starting at offset with the specified length.
+
 word="$1"
 
-reverse=""
+reversed=""
 for (( i=${#word}-1; i>=0; i-- )); do
-	reverse+="${word:$i:1}"
+	reversed+="${word:$i:1}"
 done
 
-echo "$reverse"
+echo "$reversed"
 
 # Alternative
 # reversed=$( echo "$string" | rev )
