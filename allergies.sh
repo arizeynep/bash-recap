ecdb1cb7cb572f2ae019d87a695bb94194cc4c93
Zeynep <melisaari81@gmail.com>
2026-03-04 13:26:50 +0200

    Added comments for allergies problem.

--- allergies.sh
+++ allergies.sh
@@ -17,6 +17,18 @@ allergy_score="$1"
 key_word="$2"
 allergy="$3"
 
+# Explanation of n&(-n), which extracts the lowest set bit of a number:
+# Note that -n = ~n + 1.
+# Example: 34 = 32 + 2
+# 34 = 00100010
+# -34 = ~34 + 1 = 11011101 + 1 = 11011110
+# 34&(-34) = 00100010 & 11011110 = 00000010 = 2
+# 
+# 34 - 2 = 32
+# 32 = 00100000
+# -32 = ~32 + 1 = 11011111 + 1 = 11100000
+# 32&(-32) = 00100000 & 11100000 = 00100000 = 32
+
 index=0
 while (( allergy_score > 0 )); do
 	power_list[index]=$(( allergy_score & -allergy_score ))
